215ae4935f0879a76f50abc3c0e1df3d3a554380
dudwns3020 <68069311+dudwns3020@users.noreply.github.com>
2021-07-16 21:51:55 +0900

    upload quicksort
    upload quicksort

--- quicksort.java
+++ quicksort.java
@@ -2,37 +2,47 @@ class Main {
   public static void main(String[] args) {
     int arr[] = {31, 8, 48, 73, 11, 3, 20, 29, 65, 15};
 
-    partition(arr, 0, arr.length - 1);
+    System.out.println("before");
+    for(int data : arr){
+      System.out.print(data + " ");
+    }
+    System.out.println("");
 
-    // for(int data : arr){
-    //   System.out.print(data + " ");
-    // }
+    quicksort(arr, 0, arr.length - 1);
+    
+    System.out.println("after");
+    for(int data : arr){
+      System.out.print(data + " ");
+    }
+    System.out.println("");
+    
   }
 
 
   static void partition(int a[], int p, int r) {
-    int i = 0;
-    int j = p;
+    int i = p - 1;
+    int j;
+    int x = a[r];
+    int tmp;
 
-    for(int k = 0; k < r; k++){
-      if ( a[r] <= a[p]) {
-        a[j] = a[p];
-        p++;
-        j++;
-      }
-      else {
-        int tmp = a[i];
-        a[i] = a[p];
-        a[p] = tmp;
-        i++;
-        j++;
-        p++;
+    for(j = p; j <= r-1; j++){
+      if(a[j] <= x){
+        tmp = a[++i];
+        a[i] = a[j];
+        a[j] = tmp;
       }
     }
-    int tmp = a[j];
-    for(int o = j; o < i; o--){
-      a[o] = a[o-1];
-      System.out.print(a[o]);
+    tmp = a[i+1];
+    a[i+1] = a[r];
+    a[r] = tmp;
+  }
+
+  static void quicksort(int a[], int p, int r){
+    int q;
+    if (p < r){
+      q = partition(a, p, r);
+      quicksort(a, p, q-1);
+      quicksort(a, q+1, r);
     }
   }
 }
