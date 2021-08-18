c83fc8d5c373ad1fa38996116e4a5dc38fd57334
dudwns3020 <68069311+dudwns3020@users.noreply.github.com>
2021-08-18 16:30:37 +0900

    upload Binarytree
    upload inorder

--- Binarytree.java
+++ Binarytree.java
@@ -76,12 +76,34 @@ class BinarySearchTree {
         }
         return min;
     }
+   
+    public void inorder(Node node) {
+        if(node != null) {
+            inorder(node.left);
+            System.out.print(node.data + " ");
+            inorder(node.right);
+        }
+    }
+    public void inorder() {
+        inorder(node);
+        System.out.println("");
 
+    }
 
 }
     
 public class Binarytree {
     public static void main(String[] args) {
-        Binarytree t = new Binarytree();
+        BinarySearchTree t = new BinarySearchTree();
+        
+        t.insert(4);
+        t.insert(2);
+        t.insert(1);
+        t.insert(3);
+        t.insert(6);
+        t.insert(5);
+        t.insert(7);
+
+        t.inorder();
     }
 }
