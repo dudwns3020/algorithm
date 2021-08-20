d4c0d2eeacc8ef58b07ccdb2dab5e49d9caf2807
dudwns3020 <68069311+dudwns3020@users.noreply.github.com>
2021-08-20 23:52:02 +0900

    add Redblacktree
    add Redblacktree

---
+++ Redblacktree.java
@@ -0,0 +1,14 @@
+class Redblacktree {
+    class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
