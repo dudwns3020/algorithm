a21c50073dcfd06b947aac5028dbeef64c1f6600
dudwns3020 <68069311+dudwns3020@users.noreply.github.com>
2021-08-31 23:42:12 +0900

    add select sort
    add select sort

---
+++ selectsort.java
@@ -0,0 +1,10 @@
+class sort {
+    public void select(int[] a) {
+        
+    }
+}
+public class selectsort {
+    public static void main(String[] args) {
+        int arr = {3, 8, 2, 0, 1, 9, 5, 7};
+    }
+}
