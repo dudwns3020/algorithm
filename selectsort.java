010cd76f473f414e64d9ea82ab19aba97bc76cc5
dudwns3020 <68069311+dudwns3020@users.noreply.github.com>
2021-09-01 23:49:56 +0900

    upload selectsort
    upload selectsort

--- selectsort.java
+++ selectsort.java
@@ -1,6 +1,21 @@
 class sort {
     public void select(int[] a) {
+        for(int i = a.length ; i >= 0; i--) {
+            int m = a[0];
+            for(int j = 0; j < a.length; j ++) {
+                if( a[j] > m) {
+                m = a[j];
+                }
+            }
+            swap(i, m);
+        }
         
+    }
+
+    public void swap(int a, int b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
     }
 }
 public class selectsort {
