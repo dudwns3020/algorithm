91fe85b3378b2fc0e961cbad53436f39973cc50f
dudwns3020 <68069311+dudwns3020@users.noreply.github.com>
2021-09-08 23:31:31 +0900

    upload selectsort
    upload selectsort

--- selectsort.java
+++ selectsort.java
@@ -1,25 +1,38 @@
 class sort {
     public void select(int[] a) {
-        for(int i = a.length ; i >= 0; i--) {
-            int m = a[0];
-            for(int j = 0; j < a.length; j ++) {
-                if( a[j] > m) {
-                m = a[j];
-                }
+        for(int i = a.length-1 ; i >= 0; i--) {
+            int m = 0;
+            for(int j = 0; j <= i; j ++) {
+               if( a[j] > a[m] ) {
+                    m = j;
+               }
             }
-            swap(i, m);
+            swap(a, i, m);
         }
-        
     }
 
-    public void swap(int a, int b) {
-        int tmp = a;
-        a = b;
-        b = tmp;
+    public void swap(int[] a, int b, int c) {
+        int tmp = a[b];
+        a[b] = a[c];
+        a[c] = tmp;
     }
 }
 public class selectsort {
     public static void main(String[] args) {
-        int arr = {3, 8, 2, 0, 1, 9, 5, 7};
+        sort s = new sort();
+
+        int[] arr = {3, 8, 2, 0, 1, 9, 5, 7};
+
+        for(int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        s.select(arr);
+
+        for(int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 }
