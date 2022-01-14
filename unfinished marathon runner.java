ba48da09a985f44b8ee95551747685d66aa20b41
dudwns3020 <68069311+dudwns3020@users.noreply.github.com>
2022-01-14 23:44:53 +0900

    upload unfinished marathon runner.java

--- unfinished marathon runner.java
+++ unfinished marathon runner.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         //participant : 참여한 선수 이름
@@ -5,16 +7,14 @@ class Solution {
         //answer      : 완주못한 선수 이름
         //이름은 1~20자, 동명이인 존재가능
         String answer = "";
-
+        HashMap<Integer, String> hm = new HashMap<Integer, String>();
+        
         for(String p : participant) {
-            for(String c : completion) {
-                if(p.equals(c)) {
-                    break;
-                 }
-            }
-            answer = p;
-            break;
+            hm.put(p, 1);            
         }        
+        for(String c : completion) {
+            hm.put(completion, hm.get(c)-1);
+        }
         return answer;
     }
 }
