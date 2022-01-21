18262d73118f897c0c0c8d5fd742f54328822935
dudwns3020 <dudwns3020@naver.com>
2022-01-21 18:40:12 +0900

    upload find_the_Kth.java (sort)

--- find_the_Kth.java
+++ find_the_Kth.java
@@ -1,17 +1,15 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands[0].length];
+   public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
         
-        for(int i = 0; i < commands[0].length; i++) {
+        for(int i = 0; i < commands.length; i++) {
             int[] str = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
             Arrays.sort(str);
-            int a = str[commands[i][2]-1];
-            answer[i] = a;
+            answer[i] = str[commands[i][2]-1];
         }
-        
+   
         return answer;
     }
 }
-//테스트 실패
