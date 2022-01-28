75baf079c6487c4a3f8976286169a0be8a7b33c7
dudwns3020 <dudwns3020@naver.com>
2022-01-28 22:40:21 +0900

    upload mock_exam.java (brute force)

--- mock_exam.java
+++ mock_exam.java
@@ -4,30 +4,31 @@ class Solution {
     public int[] solution(int[] answers) {
         int[] answer = {};
         
-        int[] one = new int[answers.length];
-        int[] two = new int[answers.length];
-        int[] thr = new int[answers.length];
-        
-
-        for(int i = 0; i < answers.length; i++) {
-            int[] cnt = {1, 2, 3, 4, 5};
-            one[i] = cnt[j];
-            j++;
-        }
-        
-        for(int i : one) {
-            System.out.println(i);
-        }
-        
-        /*
-        for(int i = 0; i < answers.length; i++) {
-            //2, 1, 2, 3, 2, 4, 2, 5
-        }
+        int[] s1 = {1, 2, 3, 4, 5};
+        int[] s2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] s3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
         for(int i = 0; i < answers.length; i++) {
-            //3, 3, 1, 1, 2, 2, 4, 4, 5, 5
+            if(s1[i] == answers[i]) {
+            
+            }
+            //1, 2, 3, 4, 5/ 1, 2, 3, 4, 5/ 1, 2, 3, 4, 5
+            //1, 2, 3, 4, 5/ 1, 2, 3, 4, 5/ 1, 2, 3, 4, 5
+        
+            if(s2[i] == answers[i]) {
+            
+            }
+            //2 1 2 3 2 4 2 5/ 2 1 2 3 2 4 2 5/ 2 1 2 3 2 4 2 5/ 
+            //1 2 3 4 5/ 1 2 3 4 5/ 1 2 3 4 5/ 1 2 3 4 5/ 1 2 3 4 5/
+        
+            if(s3[i] == answers[i]) {
+            
+            }
+            //3, 3, 1, 1, 2, 2, 4, 4, 5, 5/ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5
+            //1, 2, 3, 4, 5/ 1, 2, 3, 4, 5/ 1, 2, 3, 4, 5/ 1, 2, 3, 4, 5
         }
-        */
+        
+        
         return answer;
     }
 }
