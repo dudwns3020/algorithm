2684b1d145aa76c7dfd5cc75d2ba38c8287eb9d4
dudwns3020 <dudwns3020@naver.com>
2022-02-01 21:34:32 +0900

    upload mock_exam.java (brute force)

--- mock_exam.java
+++ mock_exam.java
@@ -4,30 +4,32 @@ class Solution {
     public int[] solution(int[] answers) {
         int[] answer = {};
         
+        int s = 3;
+        int[] score = new int[s];
         int[] s1 = {1, 2, 3, 4, 5};
         int[] s2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] s3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
         for(int i = 0; i < answers.length; i++) {
             if(s1[i] == answers[i]) {
-            
+                score[0] += 1;
+            }        
+            if(s2[i] == answers[i%5]) {
+                score[1] += 1;
+            }            
+            if(s3[i] == answers[i%8]) {
+                score[2] += 1;
             }
-            //1, 2, 3, 4, 5/ 1, 2, 3, 4, 5/ 1, 2, 3, 4, 5
-            //1, 2, 3, 4, 5/ 1, 2, 3, 4, 5/ 1, 2, 3, 4, 5
-        
-            if(s2[i] == answers[i]) {
-            
-            }
-            //2 1 2 3 2 4 2 5/ 2 1 2 3 2 4 2 5/ 2 1 2 3 2 4 2 5/ 
-            //1 2 3 4 5/ 1 2 3 4 5/ 1 2 3 4 5/ 1 2 3 4 5/ 1 2 3 4 5/
-        
-            if(s3[i] == answers[i]) {
-            
-            }
-            //3, 3, 1, 1, 2, 2, 4, 4, 5, 5/ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5
-            //1, 2, 3, 4, 5/ 1, 2, 3, 4, 5/ 1, 2, 3, 4, 5/ 1, 2, 3, 4, 5
         }
         
+        int hs = score[0];
+        for(int j = 0; j < s; j++) {            
+            if(hs < score [j]) {
+                hs = score[j];
+            }
+        }       
+       
+        ArrayList<Integer> list = new ArrayList<>();
         
         return answer;
     }
