0adb043c5be25b07f8d0107033506aa3183a2c6e
dudwns3020 <dudwns3020@naver.com>
2022-02-05 23:42:58 +0900

    upload find_decimal (brute force)

--- find_decimal.java
+++ find_decimal.java
@@ -1,15 +1,39 @@
 class Solution {
     public int solution(String numbers) {
         int answer = 0;
-        
-        int paper = Integer.parseInt(numbers);
-        int[] piece = new int[numbers.length()];
-            
+         
+        String[] paper = new String[numbers.length()];
+
         for(int i = 0; i < numbers.length(); i++) {
-            piece[i] = paper%10;
+            paper[i] = numbers.substring(i, i+1);
         }
-        //011 -> 11 (X)
         
+        String[] sum = new String[numbers.length() * numbers.length()];
+        int cnt = 0;
+        for(int i = 0; i < numbers.length(); i++) { 
+            for(int j = 0; j < numbers.length(); j++) { 
+                if(i == j) {
+                    sum[cnt++] = paper[i];
+                }
+                else {
+                    sum[cnt++] = paper[i] + paper[j];
+                }
+            }
+        } // 101(X)
+        
+        for(String a : sum) {
+            System.out.println(a);
+        }
+        
+        /*int[] piece = new int[sum.length];
+        
+        for(int i = 0; i < sum.length; i++) {
+            piece[i] = Integer.parseInt(sum[i]);
+        }
+        
+        for(int a : piece) {
+            System.out.println(a);
+        }*/
         return answer;
     }
 }
