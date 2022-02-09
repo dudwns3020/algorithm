0c0353d3ab4e4a47d46baa1743ebe33ddf0efa7d
dudwns3020 <dudwns3020@naver.com>
2022-02-09 21:56:11 +0900

    upload find_deciaml.java (brute force)

--- find_decimal.java
+++ find_decimal.java
@@ -1,27 +1,58 @@
+import java.util.*;
+
 class Solution {
     public int solution(String numbers) {
-        int answer = 0;
+        int answer = 0;        
         
         String[] piece = new String[numbers.length()];
-        /*
+        
         for(int i = 0; i < numbers.length(); i++) {
-            piece[i] = Integer.parseInt(numbers.substring(i, i+1));
+            piece[i] = String.valueOf(numbers.charAt(i));
         }
-        */
-        int r = piece.length;
-        String[] output = new String[r];
-        boolean[] visited = new boolean[r];
-        permutation(piece, output, visited, 0, r);
+            
+        for(int i = 0; i < numbers.length(); i++) {
+            String[] output = new String[i + 1];
+            boolean[] visited = new boolean[numbers.length()];
+            permutation(piece, output, visited, 0, i + 1);
+        }
+        
+        answer = cnt;
         
         return answer;
     }
     
-    public static void permutation(String[] arr, String[] output, boolean[] visited, int depth, int r) {
+    int cnt = 0;
+    
+    HashMap<Integer, Integer> map = new HashMap<>();
+    
+    boolean isPrime(int num) {
+        for(int i = 2; i < num; i++) {
+            if(num % i == 0 ) return false; 
+        }
+        return true;
+    }
+    
+    public void permutation(String[] arr, String[] output, boolean[] visited, int depth, int r) {
         if(depth == r) {
-            for(int i = 0; i < r; i++) {
-                System.out.print(output[i]);
+            String temp = "";
+            for(String a: output) {
+                temp += a;
             }
-            System.out.println();
+            
+            while(temp.length() > 1 && temp.charAt(0) == '0') {
+                temp = temp.substring(1);
+            }
+            
+            if(temp.equals("0") || temp.equals("1")) return;
+            
+            int result = Integer.parseInt(temp);
+            
+            map.put(result, map.getOrDefault(result, 0) + 1);
+            if(map.get(result) == 1) {
+                if(isPrime(result)) {
+                    cnt++;
+                }
+            }
             return;
         }
         
@@ -33,5 +64,5 @@ class Solution {
             visited[i] = false;
             }
         }
-    }    
+    }
 }
