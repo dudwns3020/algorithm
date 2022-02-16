c4ca9a359bc0c0712ff8ccdd9d0a46bc9d3c15f9
dudwns3020 <dudwns3020@naver.com>
2022-02-16 23:28:56 +0900

    upload crossing_trucks.java (queue)

--- crossing_trucks.java
+++ crossing_trucks.java
@@ -6,29 +6,29 @@ class Solution {
         int answer = 0;
         
         Queue<Integer> queue = new LinkedList<>();
-
+        
         int sum_weight = 0;
         
         for(int i = 0; i < truck_weights.length; i++) {
-            if(sum_weight + truck_weights[i] <= weight) {
-                queue.add(truck_weights[i]);
+            
+            if(queue.isEmpty()) {
+                queue.add(truck_weights[0]);
                 answer++;
             }
-            if(sum_weight + truck_weights[i] > weight) {
+            else if(queue.size() == bridge_length) {
+                if(queue.peek() != 0) {
+                    queue.poll();
+                }
+                else {
+                    
+                }
+            }
+            else {
                 queue.add(0);
                 answer++;
             }
         }
-        
-        /*
-        if(queue.size() == bridge_length && queue.peek() != 0) {
-            queue.poll();
-        }
-        if(queue.size() == bridge_length && queue.peek() == 0) {
-            queue.remove();
-        }
-        */
-        
+            
         return answer;
     }
 }
