66d4919aa80ec686e670fa8d94587a4ac73cce31
dudwns3020 <dudwns3020@naver.com>
2022-03-01 23:53:22 +0900

    upload stock_price.java

--- stock_price.java
+++ stock_price.java
@@ -1,29 +1,16 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = {};
         
-        Queue<Integer> queue = new LinkedList<>();
+        answer = new int[prices.length];
         
-        for(int a: prices) {
-            queue.add(a);
-        }
-        
-        answer = new int[queue.size()];
-        
-        for(int i = 0; i < prices.length; i++) {
-            int cnt = 0;
-            for(int j = i + 1; j < prices.length; j++) {
-                if(queue.peek() <= prices[j]) {
-                    cnt++;
-                }
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j]) 
+                    break;
             }
-            queue.poll();
-            answer[i] = cnt;
         }
-        
         return answer;
     }
 }
