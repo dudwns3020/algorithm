09648e8d59235b2c02a028585c7d9279251714d0
dudwns3020 <dudwns3020@naver.com>
2022-04-03 23:51:51 +0900

    upload network.java (dfs)

--- codingtest_practice/network.java
+++ codingtest_practice/network.java
@@ -1,19 +1,24 @@
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean visited = new boolean[n];
-               
+        boolean[] visited = new boolean[n];
+        
         for(int i = 0; i < n; i++) {
             if(visited[i] == false){
-                dfs(n, computers, visited);
+                answer++;
+                dfs(i, computers, visited);
             }
-            
+        }
         return answer;
     }
-    
+        
     public void dfs(int n, int[][] computers, boolean[] visited) {
         visited[n] = true;
-            
+        
+        for(int i = 0; i < computers.length; i++) {
+            if (visited[i] == false && computers[n][i] == 1) {
+                dfs(i, computers, visited);
+            }
         }
     }
 }
