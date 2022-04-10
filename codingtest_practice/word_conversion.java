b9b05afe9028eb17b054752bde6efb3319a33a15
dudwns3020 <dudwns3020@naver.com>
2022-04-10 23:24:43 +0900

    upload word_conversion.java (dfs)

--- codingtest_practice/word_conversion.java
+++ codingtest_practice/word_conversion.java
@@ -1,32 +1,38 @@
 class Solution {
+    static int answer = 0;
+    
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
+        answer = 51;
         
         boolean[] visited = new boolean[words.length];
         
-        dfs(begin, target, words, visited);
+        dfs(begin, target, words, visited, 0);
         
+        if(answer == 51) {
+            answer = 0;
+        }        
         return answer;
     }
     
-    public int dfs(String begin, String target, String[] words, boolean[] visited) {
-       int no = 0;
+    public void dfs(String begin, String target, String[] words, boolean[] visited, int cnt) {
+        if(begin.equals(target)) {
+            if(answer > cnt) {
+                answer = cnt;    
+            }            
+            return;
+        }
+        
         for(int i = 0; i < words.length ; i++) {
-            int cnt = 0;
+            int same = 0;
             for(int j = 0; j < begin.length() ; j++) {
                 if(begin.charAt(j) == words[i].charAt(j)) {
-                    cnt++;
+                    same++;
                 }
             }
-            if(cnt == begin.length() - 1) {
-                no++;
-                begin = words[i];
-                if(begin == target) {
-                }
+            if(visited[i] && same == begin.length() - 1) {
                 visited[i] = true;
-                dfs(begin, target, words, visited);
+                dfs(begin, target, words, visited, cnt + 1);
             }
         }
-        return no;
     }
 }
