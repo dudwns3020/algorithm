35cda89bc05001411abcb6aae65d4041b03f8dbb
dudwns3020 <dudwns3020@naver.com>
2022-04-11 22:07:43 +0900

    word_conversion.java (dfs)

--- codingtest_practice/word_conversion.java
+++ codingtest_practice/word_conversion.java
@@ -1,12 +1,13 @@
 class Solution {
-    static int answer = 0;
+    int answer;
+    boolean[] visited;
     
     public int solution(String begin, String target, String[] words) {
         answer = 51;
         
-        boolean[] visited = new boolean[words.length];
+        visited = new boolean[words.length];
         
-        dfs(begin, target, words, visited, 0);
+        dfs(begin, target, words, 0);
         
         if(answer == 51) {
             answer = 0;
@@ -14,25 +15,32 @@ class Solution {
         return answer;
     }
     
-    public void dfs(String begin, String target, String[] words, boolean[] visited, int cnt) {
+    public void dfs(String begin, String target, String[] words, int cnt) {
         if(begin.equals(target)) {
             if(answer > cnt) {
-                answer = cnt;    
-            }            
+                answer = cnt;
+            }
             return;
-        }
-        
+        }        
         for(int i = 0; i < words.length ; i++) {
-            int same = 0;
-            for(int j = 0; j < begin.length() ; j++) {
-                if(begin.charAt(j) == words[i].charAt(j)) {
-                    same++;
-                }
-            }
-            if(visited[i] && same == begin.length() - 1) {
+            if(!visited[i] && check(begin, words[i])) {
                 visited[i] = true;
-                dfs(begin, target, words, visited, cnt + 1);
+                dfs(words[i], target, words, cnt + 1);
+                visited[i] = false;
             }
         }
+    }
+    
+    public boolean check (String begin, String target) {
+        int same = 0;
+        for(int j = 0; j < begin.length() ; j++) {
+            if(begin.charAt(j) == target.charAt(j)) {
+                same++;
+            }
+        }
+        if (same == begin.length() - 1) {
+            return true;
+        }
+        return false;
     }
 }
