8c4de394a1b859325c5477eb03aafc3a34a714b2
dudwns3020 <dudwns3020@naver.com>
2022-05-29 23:27:44 +0900

    upload best_album.java

--- codingtest_practice/best_album.java
+++ codingtest_practice/best_album.java
@@ -4,31 +4,42 @@ class Solution {
     public int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
         
-        HashMap<String, Integer> genres_list = new HashMap<>();
-        HashMap<Integer, Integer> plays_list = new HashMap<>();
-        
+        HashMap<String, Integer> genresList = new HashMap<>();
+        HashMap<Integer, Integer> playsList = new HashMap<>();
+
         for(int i = 0; i < genres.length; i++) {
-            genres_list.put(genres[i], genres_list.getOrDefault(genres[i], 0) + plays[i]);
+            genresList.put(genres[i], genresList.getOrDefault(genres[i], 0) + plays[i]);
         }
         
-        // for(String s: genres_list.keySet()) {
-        //     System.out.println(s + ", " +genres_list.get(s));
-        // }
-        for(String s: genres_list.keySet()) {
+        List<String> genresKeyList = new ArrayList<>(genresList.keySet());
+        genresKeyList.sort((s1, s2) -> genresList.get(s2).compareTo(genresList.get(s1)));
+        
+        List<Integer> answerList = new ArrayList<>();
+        
+        for(String s: genresKeyList) {
             for(int i = 0; i < genres.length; i++) {
-                if(genres[i].equals(s)) {
-                    plays_list.put(i, plays[i]);
+                if(s.equals(genres[i])) {
+                    playsList.put(i, plays[i]);
                 }
             }
+            List<Integer> keyList = new ArrayList<>(playsList.keySet());
+            keyList.sort((s1, s2) -> playsList.get(s2).compareTo(playsList.get(s1)));
             
-            //vaule
-            
-            for(int n: plays_list.keySet()) {
-                System.out.println(genres[n] + n + ", " + plays_list.get(n));
+            int j = 0;
+            for(int c : keyList) {
+                if(j > 1) {
+                    break;
+                }
+                answerList.add(c);
+                j++;
             }
-            plays_list.clear();
+            playsList.clear();
         }
         
+        answer = new int[answerList.size()];
+        for(int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
+        }
         return answer;
     }
 }
