e9eb8a201c55d4a18b37ac2ba83ef6fa8cbd586a
dudwns3020 <dudwns3020@naver.com>
2022-06-02 00:00:07 +0900

    add travel_route .java

---
+++ codingtest_practice/travel_route .java
@@ -0,0 +1,10 @@
+class Solution {
+    public String[] solution(String[][] tickets) {
+        boolean[] visited;
+
+        public void dfs(int c, String s, String answer, String[][]ticktes) {
+        }
+        
+        return;
+    }
+}
