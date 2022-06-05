2bac206dcf55a68a551d2e5349f303a7ff136ff7
dudwns3020 <dudwns3020@naver.com>
2022-06-05 23:47:12 +0900

    upload travel_route.java

--- codingtest_practice/travel_route .java
+++ codingtest_practice/travel_route .java
@@ -1,29 +1,24 @@
+import java.util.*;
+
 class Solution {
     public String[] solution(String[][] tickets) {
         String[] answer = {};
-        boolean[] visited = new boolean[tickets.length];
-        
+        boolean[] visited = new boolean[tickets.length];   
         dfs(0, "ICN", visited, tickets);
-        
+
         return answer;
     }
     
     public void dfs(int c, String s, boolean[] visited, String[][] tickets) {
-        if(c == visited.length) {
+        if(c == tickets.length) {
             return;
         }
-        String end = null;
         for(int i = 0; i < tickets.length; i++) {
-            if(s.equals(tickets[i][0])) {
-                if(end !=null) {
-                    int a = end.compareTo(tickets[i][1]);
-                    if(a < 0) {
-                        end = tickets[i][1];
-                    }
-                }
-                end = tickets[i][1];
+            if(!visited[i] && s.equals(tickets[i][0])) {
+                visited[i] = true;
+                dfs(c, tickets[i][1], visited, tickets);
+                visited[i] = false;    
             }
         }
-        
-    }
+   }
 }
